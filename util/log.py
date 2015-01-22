485bfd97d1b305ad0944192d4ea8c77a479936ad
John Barbero Unenge <john.barbero.unenge@gmail.com>
2015-01-22 13:24:37 +0100

    Add note and fatal to Log

--- util/log.py
+++ util/log.py
@@ -20,8 +20,17 @@ class Log:
     Log.print_msg("Warning", msg, Colors.YELLOW_FG, new_line)
 
   @classmethod
+  def note(cls, msg, new_line = True):
+    Log.print_msg("Note", msg, Colors.YELLOW_FG, new_line)
+
+  @classmethod
   def err(cls, msg, new_line = True):
     Log.print_msg("Error", msg, Colors.RED_FG, new_line)
+
+  @classmethod
+  def fatal(cls, msg, new_line = True):
+    Log.print_msg("Fatal", msg, Colors.RED_FG, new_line)
+    exit(1)
 
   @classmethod
   def raw(cls, msg, new_line = True):
